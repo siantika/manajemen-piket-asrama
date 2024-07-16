797dad1386ef84330bb7bbcf5efea381569e0d34
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-07-16 15:11:51 +0800

    initial structure

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create Express application
+const app = express();
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined')); // Logging HTTP requests
+app.use(helmet()); // Set security headers
+app.use(cors()); // Enable CORS
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes setup
+// import indexRouter from './routes/index';
+// import usersRouter from './routes/users';
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.get('/', (req:any, res:any) => {
+    res.send('Hello asasdsaddsa!');
+});
+
+// Error handling middleware
+app.use((err:any, req:any, res:any, next:any) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
