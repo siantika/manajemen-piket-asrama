7f28f212a0f2e9f6f21d37e4b7ca158a0c5a3950
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-07-19 18:44:02 +0800

    Fix bootstrap and front end library files configuration

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response, NextFunction } from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import cors from 'cors';
-import { logger } from './src/utils/logger';
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import { logger } from "./src/utils/logger";
+import { LihatDaftarPiket } from "./src/apps/lihatDaftarPiket/types";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -15,26 +16,40 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet()); // Set security headers
-app.use(cors()); // Enable CORS
+app.use(cors());
 
 // Serve static files
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use(express.static(path.join(__dirname, "public")));
 
+// Set view engine to EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Sample data
+const piketData = [
+  { nama: "Sian", tempat: "Dapur" },
+  { nama: "Arbi", tempat: "Kamar Mandi" },
+  { nama: "Yoga", tempat: "Tamu" },
+  { nama: "Agus", tempat: "Dapur" },
+];
+
+const tanggalPiket = "Minggu, 17 Juli 2024";
 
 // Routes setup
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+app.get("/", (req: Request, res: Response) => {
+  const data: LihatDaftarPiket = { piketData, tanggalPiket };
+  res.render("home", data);
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`Request URL: ${req.url}`);
+  next();
 });
 
 // Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
   logger.error(err.stack);
 });
 
---
+++ src/apps/lihatDaftarPiket/types.ts
@@ -0,0 +1,9 @@
+export interface Piket{
+    nama: string;
+    tempat: string;
+}
+
+export interface LihatDaftarPiket{
+    piketData: Piket[];
+    tanggalPiket: string;
+}
