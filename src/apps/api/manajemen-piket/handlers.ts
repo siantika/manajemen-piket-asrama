7da41477f0ddf5648bdae0b6374b44c810e244b3
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-07-27 23:26:35 +0800

    Add place endpoints

--- src/apps/api/manajemen-piket/handlers.ts
+++ src/apps/api/manajemen-piket/handlers.ts
@@ -7,6 +7,7 @@ import {
 } from "./member";
 import { StatusCodes } from "http-status-codes";
 import { logger } from "../../../utils/logger";
+import { addPlace, deletePlace, readAllPlaces, updatePlace } from "./tempat";
 
 export const addMemberHandler = async (req: Request, res: Response) => {
   const { memberName } = req.body;
@@ -63,6 +64,72 @@ export const deleteMemberHandler = async (req: Request, res: Response) => {
     const deletedMember = await deleteMember(memberId);
     res.status(StatusCodes.OK).json({
       message: "Member deleted successfully",
+      data: deletedMember,
+    });
+  } catch (error: any) {
+    logger.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message,
+    });
+  }
+};
+
+// tempat
+export const addPlaceHandler = async (req: Request, res: Response) => {
+  const { placeName } = req.body;
+
+  try {
+    const newPlace = await addPlace(placeName);
+    res.status(StatusCodes.CREATED).json({
+      message: "Place added successfully",
+      data: newPlace,
+    });
+  } catch (error: any) {
+    logger.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message,
+    });
+  }
+};
+
+export const readAllPlacesHandler = async (req:Request, res: Response) => {
+  try {
+    const places = await readAllPlaces();
+    res.status(StatusCodes.OK).json({
+      data: places,
+    });
+  } catch (error: any) {
+    logger.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message,
+    });
+  }
+};
+
+export const updatePlaceHandler = async (req: Request, res: Response) => {
+  const { placeId, placeName } = req.body;
+
+  try {
+    const updatedPlace = await updatePlace(placeId, placeName);
+    res.status(StatusCodes.OK).json({
+      message: "Place updated successfully",
+      data: updatedPlace,
+    });
+  } catch (error: any) {
+    logger.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deletePlaceHandler = async (req: Request, res: Response) => {
+  const placeId = req.params.placeId;
+
+  try {
+    const deletedMember = await deletePlace(placeId);
+    res.status(StatusCodes.OK).json({
+      message: "Place deleted successfully",
       data: deletedMember,
     });
   } catch (error: any) {
--- src/apps/api/manajemen-piket/routes.ts
+++ src/apps/api/manajemen-piket/routes.ts
@@ -4,6 +4,10 @@ import {
   updateMemberHandler,
   deleteMemberHandler,
   readAllMemberHandler,
+  addPlaceHandler,
+  readAllPlacesHandler,
+  updatePlaceHandler,
+  deletePlaceHandler,
 } from "./handlers";
 import { auth } from "../../../middlewares/auth";
 
@@ -13,5 +17,10 @@ router.post("/members", auth, addMemberHandler);
 router.get("/members", readAllMemberHandler);
 router.put("/members", auth, updateMemberHandler);
 router.delete("/members/:memberId", auth, deleteMemberHandler);
+
+router.post("/places", auth, addPlaceHandler);
+router.get("/places", readAllPlacesHandler);
+router.put("/places", auth, updatePlaceHandler);
+router.delete("/places/:placeId", auth, deletePlaceHandler);
 
 export default router;
