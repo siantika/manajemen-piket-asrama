ba60b3bf29e5e6c1d94498001770153d1ef490fd
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-07-27 21:07:16 +0800

    fix routes and urlencode set to be true

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -7,23 +7,21 @@ import viewsRoute from "../routes/views";
 import apiRoutes from "../routes/api";
 
 dotenv.config();
-
 const app = express();
 
 // Middleware setup
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet()); // Set security headers
 app.use(cors());
-// IMPORTANT: api routes must prior the views routes!
-app.use(apiRoutes);
 
 // Front End rendering server middlewares
-// Serve static files
 app.use(express.static(path.join(__dirname, "../../public")));
-// Set view engine to EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../../views"));
+
+// Register routes
+app.use(apiRoutes);
 app.use(viewsRoute);
 
 export default app;
