d155d6c40d94b8365c216046b66affcf415bcd28
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-07-28 12:54:41 +0800

    Disable Logging

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,6 +9,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER!,
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME,
+  logging: false,
 });
 
 export default sequelize;
