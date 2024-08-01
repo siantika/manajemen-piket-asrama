38f073afd45da798f98f1ad53dcf9a76cb50ee58
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-01 21:37:00 +0800

    Fix error connection to database
    Implemented relative path to .env file

--- src/apps/api/buat-jadwal/helpers.ts
+++ src/apps/api/buat-jadwal/helpers.ts
@@ -3,7 +3,8 @@ import RiwayatPiket, { IRiwayatPiket } from "../../../models/riwayat-piket";
 import Tempat from "../../../models/tempat";
 import { logger } from "../../../utils/logger";
 import CONST from "../../../config/consts";
-import { IGeneratedSchedule } from "./interfaces";
+import { IGeneratedSchedule, ISchedule } from "./interfaces";
+import Joi from "joi";
 
 export const getMembers = async (): Promise<Member[]> => {
   try {
@@ -92,3 +93,52 @@ export const createScheduleEntry = (
     tanggalPiket: new Date(),
   };
 };
+
+export const isImportantPlaceFulfilled = () => {};
+
+export const isOneCyclePicketAchieved = () => {};
+
+export const getBackupPeople = () => {};
+
+export const saveRecapPicket = () => {};
+
+export const saveGeneratedSchedule = async (schedules: ISchedule) => {
+  try {
+    const { error } = scheduleSchema.validate({
+      placeId: schedules.tempatId,
+      memberId: schedules.penghuniId,
+      statusPiket: schedules.statusPiket,
+      tanggalPiket: schedules.tanggalPiket,
+    });
+
+    if (error) {
+      logger.error("Validation error:", error.details);
+      throw new Error(
+        `Validation error: ${error.details.map((d) => d.message).join(", ")}`
+      );
+    }
+    const statusPiketReceived = schedules.statusPiket;
+    // Make sure statusPiket value is "belum" or "sudah"
+    if (!(statusPiketReceived === "sudah" || statusPiketReceived === "belum")) {
+      throw new Error("Invalid statusPiket value");
+    }
+    
+    await RiwayatPiket.create({
+      tempatId: schedules.tempatId,
+      penghuniId: schedules.penghuniId,
+      statusPiket: statusPiketReceived,
+      tanggalPiket: schedules.tanggalPiket,
+    });
+    
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+};
+
+const scheduleSchema = Joi.object({
+  placeId: Joi.string().required(),
+  memberId: Joi.string().required(),
+  statusPiket: Joi.string().valid("sudah", "belum").required(),
+  tanggalPiket: Joi.date().required(),
+});
