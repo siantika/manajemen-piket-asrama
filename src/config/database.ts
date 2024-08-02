b8d0ac4b2a20de4172790c0cc887d611f9a88798
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-02 11:33:28 +0800

    Remove console.log in database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,17 +2,9 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 import path from "path";
 
-
 // relative path to .env file (Important)
 const envPath = path.resolve(__dirname, "../../.env");
 dotenv.config({ path: envPath });
-
-console.log({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
 
 const sequelize = new Sequelize({
   dialect: "postgres",
@@ -22,14 +14,5 @@ const sequelize = new Sequelize({
   database: process.env.DB_NAME,
   logging: false,
 });
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
 
 export default sequelize;
