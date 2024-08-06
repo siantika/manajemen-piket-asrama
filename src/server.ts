99aa15c6faad153623f7abb0dfd98d5bc0538c5b
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-07 00:27:23 +0800

    Implements cron job : generate schedule mingguan to main app

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+import { startCronjobs } from "./apps/api/cron-jobs/jadwal-mingguan";
 import app from "./config/app-config";
 import CONST from "./config/consts";
 import { errorHandler } from "./middlewares/error-handler";
 
 app.use(errorHandler);
 
+startCronjobs();
+
 // Start the server
 app.listen(CONST.PORT, () => {
   console.log(`Server is running on http://localhost:${CONST.PORT}`);
 });
+
