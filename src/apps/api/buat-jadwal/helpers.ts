f1e78bb374b9029f0aa774a3257df06c1213af92
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-06 14:00:20 +0800

    Add shuffle array
    Make places and members shuffled before assigment

--- src/apps/api/buat-jadwal/helpers.ts
+++ src/apps/api/buat-jadwal/helpers.ts
@@ -176,6 +176,14 @@ export const saveRecapPicket = async (schedules: IRekapPiket) => {
   }
 };
 
+export const shuffleArray = (array: any) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 const scheduleSchema = Joi.object({
   placeId: Joi.string().required(),
   memberId: Joi.string().required(),
