b9546e18d5a00fac85e2cc8db371fe65b28fd4d7
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-08 11:59:04 +0800

    Register login route for admin in Front end

--- src/routes/views.ts
+++ src/routes/views.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { renderHomePage } from "../apps/frontend/home";
 import { renderLoginAdmin } from "../apps/frontend/login-admin/login-admin";
+import { loginAdminHandler } from "../apps/frontend/login-admin/handlers";
+
 
 const router = Router();
 
 router.get("/", renderHomePage);
 router.get("/login-admin", renderLoginAdmin);
+router.post("/login", loginAdminHandler);
 
 export default router;
