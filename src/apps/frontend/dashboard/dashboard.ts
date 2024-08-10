bfccf638e7d1d56bb9d21dc648da62764e9861b7
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-10 13:15:14 +0800

    make it read from DB

--- src/apps/frontend/dashboard/dashboard.ts
+++ src/apps/frontend/dashboard/dashboard.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
+import { readAllMembers } from "../../api/manajemen-piket/member";
 
 export const renderDashboardAdmin = (req: Request, res: Response) => {
   res.render("dashboard-admin/main");
 };
 
-export const renderManajemenOrang = (req: Request, res: Response) => {
-  const orang = [{ nama: "sian" }, { nama: "arbi" }];
+export const renderManajemenOrang = async (req: Request, res: Response) => {
+  // const orang = [{ nama: "sian" }, { nama: "arbi" }];
+  const orang = await readAllMembers();
   res.render("dashboard-admin/manajemen-orang", {
     title: "Manajemen Orang",
     orang,
