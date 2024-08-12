7fa0951a6fe4d7d8beb1537558f47479b404f5a7
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-12 12:22:42 +0800

    add manajemen piket sekarang feature (FE)

--- src/apps/frontend/dashboard/dashboard.ts
+++ src/apps/frontend/dashboard/dashboard.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { readAllMembers } from "../../api/manajemen-piket/member";
 import { readAllPlaces } from "../../api/manajemen-piket/tempat";
+import path from "path";
 
 export const renderDashboardAdmin = (req: Request, res: Response) => {
   res.render("dashboard-admin/main", { currentPage: "main" });
@@ -21,5 +22,24 @@ export const renderManajemenTempat = async (req: Request, res: Response) => {
     title: "Manajemen Tempat",
     tempat,
     currentPage: "tempat",
+  });
+};
+
+export const renderManajemenPiketSekarang = async (
+  req: Request,
+  res: Response
+) => {
+  const piket = [
+    { id: "99yasdh2", tanggal_piket: new Date(), nama: "Sian" },
+    { id: "99yasdh3", tanggal_piket: new Date(), nama: "Arbi" },
+  ];
+  console.log(
+    "Rendering view from:",
+    path.join(__dirname, "views/dashboard-admin/manajemen-piket-sekarang")
+  );
+  res.render("dashboard-admin/manajemen-piket-sekarang", {
+    piket,
+    title: "Manajemen Tempat",
+    currentPage: "piket",
   });
 };
--- src/routes/views.ts
+++ src/routes/views.ts
@@ -4,6 +4,7 @@ import { renderLoginAdmin } from "../apps/frontend/login-admin/login-admin";
 import {
   renderDashboardAdmin,
   renderManajemenOrang,
+  renderManajemenPiketSekarang,
   renderManajemenTempat,
 } from "../apps/frontend/dashboard/dashboard";
 
@@ -15,5 +16,6 @@ router.get("/dashboard-admin", renderDashboardAdmin);
 router.post("/login", renderLoginAdmin);
 router.get("/manajemen-orang", renderManajemenOrang);
 router.get("/manajemen-tempat", renderManajemenTempat);
+router.get("/manajemen-piket", renderManajemenPiketSekarang);
 
 export default router;
