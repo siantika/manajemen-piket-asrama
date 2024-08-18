f6defe5e0fa6f727f29c8216c6000c3cdb77380e
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-19 01:38:00 +0800

    add ssl to database communication

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 import path from "path";
 
-// relative path to .env file (Important)
+// Relative path to .env file (Important)
 const envPath = path.resolve(__dirname, "../../.env");
 dotenv.config({ path: envPath });
 
@@ -13,6 +13,12 @@ const sequelize = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // This setting might be necessary if you're connecting to a database with a self-signed certificate
+    },
+  },
 });
 
 export default sequelize;
