56397b3b6f5bad4af8f71c6c974e69947a0bc06f
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-18 22:29:07 +0800

    Replace Schedule time to correct time

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -12,10 +12,9 @@ const CONST = {
     SUDAH: "sudah" as "sudah",
   },
   CRON_JOB: {
-    // GENERATE_SCHEDULE_TIME: "0 23 * * 5",
-    // POST_SCHEDULE_TIME: "59 23 * * 0",
-    GENERATE_SCHEDULE_TIME: "06 22 * * *",
-    POST_SCHEDULE_TIME: "08 22 * * *",
+    GENERATE_SCHEDULE_TIME: "0 23 * * 5",
+    POST_SCHEDULE_TIME: "59 23 * * 0",
+
   },
 };
 
