07bee63996c41d48253636013e02f6a683560ece
I Putu Pawesi Siantika <csiantika@gmail.com>
2024-08-20 12:22:02 +0800

    Fix main.ejs

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
-import { startCronjobs } from "./apps/api/cron-jobs/jadwal-mingguan";
 import app from "./config/app-config";
 import CONST from "./config/consts";
 
-startCronjobs();
-
-// // Start the server
-// app.listen(CONST.PORT, () => {
-//   console.log(`Server is running on http://localhost:${CONST.PORT}`);
-// });
+// Start the server
+app.listen(CONST.PORT, () => {
+  console.log(`Server is running on http://localhost:${CONST.PORT}`);
+});
 
 export default app;
